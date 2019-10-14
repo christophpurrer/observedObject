93bba65247f07fea7b60ad933d9b5ec97fedd95c
Christoph Purrer <chpurrer@fb.com>
2019-10-13 20:36:57 -0700

    Init

---
+++ observedObject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  observedObject
+//
+//  Created by Christoph Purrer on 10/13/19.
+//  Copyright © 2019 Christoph Purrer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
